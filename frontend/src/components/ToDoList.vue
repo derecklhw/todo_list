9e1fe95bdee939cfccf6a899ab9260900e37651f
dereck lhw <dereck.lamhonwah@gmail.com>
2024-07-04 19:55:31 +0400

    Styles: final layout

--- frontend/src/components/ToDoList.vue
+++ frontend/src/components/ToDoList.vue
@@ -1,17 +1,17 @@
 <template lang="">
   <div class="container vh-100 vw-100">
     <div class="d-flex justify-content-center">
-      <h1 class="m-3">My ToDo List</h1>
+      <h1 class="m-3">The ToDo List 📝</h1>
     </div>
 
     <ul class="nav mb-2 mt-2">
       <li class="nav-item">
         <a class="nav-link active" aria-current="page" href="#"
-          >TODO <span class="badge bg-secondary">4</span></a
+          >Pending <span class="badge bg-secondary">4</span></a
         >
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Completed</a>
+        <a class="nav-link" href="#">Completed <span class="badge bg-secondary">2</span></a>
       </li>
     </ul>
 
@@ -29,7 +29,7 @@
     </div>
 
     <div class="overflow-auto h-75">
-      <div class="card w-100">
+      <div class="card w-100" style="background-color: rgb(245 245 245)">
         <div class="card-body">
           <h5 class="card-title mb-3">Card title</h5>
           <div class="row">
@@ -59,7 +59,7 @@
         </div>
       </div>
       <br />
-      <div class="card w-100">
+      <div class="card w-100" style="background-color: rgb(245 245 245)">
         <div class="card-body">
           <h5 class="card-title mb-3">Card title</h5>
           <div class="row">
@@ -89,38 +89,7 @@
         </div>
       </div>
       <br />
-      <div class="card w-100">
-        <div class="card-body">
-          <h5 class="card-title mb-3">Card title</h5>
-          <div class="row">
-            <p class="card-text col-md-9">
-              With supporting text below as a natural lead-in to additional content.
-            </p>
-            <div class="col-md-3">
-              <div class="row justify-content-start justify-content-md-end align-items-center">
-                <a href="#" class="btn btn-primary mx-2 col-auto" aria-label="Delete">
-                  <i class="bi bi-trash3"></i>
-                </a>
-                <a href="#" class="btn btn-primary mx-2 col-auto" aria-label="Edit">
-                  <i class="bi bi-pencil"></i>
-                </a>
-                <div class="form-check form-switch col-auto mx-2">
-                  <input
-                    class="form-check-input"
-                    type="checkbox"
-                    role="switch"
-                    id="flexSwitchCheckDefault"
-                    aria-label="Toggle feature"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <br />
-      <div class="card w-100">
+      <div class="card w-100" style="background-color: rgb(245 245 245)">
         <div class="card-body">
           <h5 class="card-title mb-3">Card title</h5>
           <div class="row">
@@ -151,7 +120,38 @@
       </div>
 
       <br />
-      <div class="card w-100">
+      <div class="card w-100" style="background-color: rgb(245 245 245)">
+        <div class="card-body">
+          <h5 class="card-title mb-3">Card title</h5>
+          <div class="row">
+            <p class="card-text col-md-9">
+              With supporting text below as a natural lead-in to additional content.
+            </p>
+            <div class="col-md-3">
+              <div class="row justify-content-start justify-content-md-end align-items-center">
+                <a href="#" class="btn btn-primary mx-2 col-auto" aria-label="Delete">
+                  <i class="bi bi-trash3"></i>
+                </a>
+                <a href="#" class="btn btn-primary mx-2 col-auto" aria-label="Edit">
+                  <i class="bi bi-pencil"></i>
+                </a>
+                <div class="form-check form-switch col-auto mx-2">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    role="switch"
+                    id="flexSwitchCheckDefault"
+                    aria-label="Toggle feature"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <br />
+      <div class="card w-100" style="background-color: rgb(245 245 245)">
         <div class="card-body">
           <h5 class="card-title mb-3">Card title</h5>
           <div class="row">
@@ -218,24 +218,23 @@
 <script setup lang="ts"></script>
 <style scoped>
 .form-switch .form-check-input {
-  width: 2.4rem;
-  height: 2.4rem;
-  background-color: #ff0000;
+  width: 3rem;
+  height: 2rem;
+  background-color: #ff9501;
   border-radius: 10%;
   border-color: rgba(0, 0, 0, 0.25);
-  background-image: url("data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='-4 -4 8 8'><path stroke='%23fff' stroke-width='1.5' d='M-2,-2 L2,2 M2,-2 L-2,2'/></svg>");
+  background-image: url("data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='-4 -4 8 8'%3e%3ccircle r='3' fill='rgba(255,255,255,1.0)'/></svg>");
   background-size: 75% 75%;
-  background-position: center;
 }
 .form-switch .form-check-input:focus {
   outline: 0;
   box-shadow: 0 0 0 0 rgba(0, 0, 0, 0);
 }
 .form-switch .form-check-input:checked {
+  border-color: rgba(0, 0, 0, 0.25);
+
   background-color: #30d158;
-  border: none;
   background-image: url("data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8'><path stroke='%23fff' stroke-width='1.5' fill='none' d='M1,4 L3,6 L7,2'/></svg>");
   background-size: 75% 75%;
-  background-position: center;
 }
 </style>
